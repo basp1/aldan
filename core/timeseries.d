0d287230511f8e660078eaa79e07fe28b0a0c63c
basp <akovn@yandex.ru>
2019-07-26 10:14:28 +0300

    Add 'get', 'getExact', 'interval' methods

--- core/timeseries.d
+++ core/timeseries.d
@@ -74,17 +74,87 @@ class TimeSeries(T)
 			items.insertAfter(items[index..index], item);
 		}
 	}
+
+
+	Tuple!(Time,T) get(Time time) 
+	{
+        int index = lowerBound(times, time);
+
+		enforce!DateTimeException(index >= 0 && index < length());
+
+        return tuple(times[index], items[index]);
+    }
+
+	bool getExact(Time time, ref T item) 
+	{
+        auto x = get(time);
+
+		if(x[0] != time)
+		{
+			return false;
+		}
+		else
+		{
+			item = x[1];
+			return true;
+		}
+    }
+
+	typeof(this) interval(Interval!Time interval) 
+	{
+		auto ts = new TimeSeries!T();
+
+		int index = lowerBound(times, interval.begin());
+
+		int n = length();
+		for (int i = index; i < n && times[i] < interval.end(); i++) 
+		{
+			ts.insert(times[i], items[i]);
+		}
+
+		return ts;
+	}
 }
 
 unittest
 {
 	auto ts = new TimeSeries!int();
-	
+
 	assert(0 == ts.length());
 
-	ts.append(DateTime(2000, 3, 17, 10, 30, 0), 1);
-	assert(1 == ts.length());
+	auto time1 = DateTime(2000, 3, 17, 10, 30, 0);
+	{
+		ts.append(time1, 10);
 
-	ts.append(DateTime(2000, 3, 17, 10, 31, 0), 2);
-	assert(2 == ts.length());
+		assert(1 == ts.length());
+		assert(10 == ts.get(time1)[1]);
+
+		int x;
+		assert(ts.getExact(time1, x));
+		assert(10 == x);
+
+		auto t = time1;
+		t.roll!"minutes"(-1);
+		assert(10 == ts.get(t)[1]);
+
+		assert(!ts.getExact(t, x));
+	}
+
+	auto time2 = DateTime(2000, 3, 17, 10, 31, 0);
+	{
+		ts.append(time2, 20);
+
+		assert(2 == ts.length());
+		assert(20 == ts.get(time2)[1]);
+
+		int x;
+		assert(ts.getExact(time2, x));
+		assert(20 == x);
+
+		auto t = time1;
+		t.roll!"seconds"(25);
+		assert(20 == ts.get(t)[1]);
+
+		assert(!ts.getExact(t, x));
+	}
 }
