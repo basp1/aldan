850264afd56d53cb890393a2381b21413ed77f8f
basp <akovn@yandex.ru>
2019-07-30 11:32:44 +0300

    Add 'Node' struct

--- decisions/id3.d
+++ decisions/id3.d
@@ -4,37 +4,55 @@ import std.container.array;
 import std.math;
 import std.uuid;
 
+import core.tree;
 import decisions.dataset;
 import decisions.feature;
 
-class Id3
+class Id3(T)
 {
+    struct Node
+    {
+        enum Type
+        {
+            Tag,
+            Feature,
+            Leaf
+        }
+
+        Type type;
+        union
+        {
+            Tag tag;
+            Feature feature;
+            Array!T* outputs;
+        }
+    }
+
     this()
-	{
+    {
     }
 
     static double entropy(Array!Feature features)
-	{
+    {
         double[UUID] probs;
 
-		foreach (feature ; features)
-		{
+        foreach (feature; features)
+        {
             if (feature.id !in probs)
-			{
+            {
                 probs[feature.id] = 0;
             }
             probs[feature.id] += 1;
         }
 
-        double count = features.length;
-        foreach (id ; probs.byKey())
-		{
-            probs[id] /= count;
+        foreach (id; probs.byKey())
+        {
+            probs[id] /= features.length;
         }
 
         double sum = 0;
-        foreach (probability ; probs)
-		{
+        foreach (probability; probs)
+        {
             sum += -probability * log(probability) / LN2;
         }
 
