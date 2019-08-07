2530cde8ff50782821f2d818278b2097368df73e
basp <akovn@yandex.ru>
2019-08-07 15:21:33 +0300

    Add min/max to 'variable.new' call

--- test/fuzzy/rule_base.lua
+++ test/fuzzy/rule_base.lua
@@ -10,15 +10,15 @@ local test = {}
 local tol = 1e-8
 
 test[#test + 1] = function()
-    local a1 = variable.new("a1")
+    local a1 = variable.new("a1", 0, 2)
     local a11 = a1:add("a11", fuzzyset.point(1, 0.7))
     local a12 = a1:add("a12", fuzzyset.point(1, 0.6))
 
-    local a2 = variable.new("a2")
+    local a2 = variable.new("a2", 0, 2)
     local a21 = a2:add("a21", fuzzyset.point(2, 0.3))
     local a22 = a2:add("a22", fuzzyset.point(2, 0.8))
 
-    local b = variable.new("b")
+    local b = variable.new("b", 0, 10)
     local b1 = b:add("b1", fuzzyset.point(8))
     local b2 = b:add("b2", fuzzyset.point(4))
 
@@ -34,7 +34,7 @@ test[#test + 1] = function()
 end
 
 test[#test + 1] = function()
-    local temp = variable.new("temperature")
+    local temp = variable.new("temperature", 0, 100)
 
     local cold = temp:add("cold", fuzzyset.linear({ [0] = 5, 10 }, { [0] = 1, 0 }))
     local cool = temp:add("cool", fuzzyset.linear({ [0] = 5, 12, 17 }, { [0] = 0, 1, 0 }))
@@ -42,7 +42,7 @@ test[#test + 1] = function()
     local warm = temp:add("warm", fuzzyset.linear({ [0] = 20, 26, 32 }, { [0] = 0, 1, 0 }))
     local hot = temp:add("hot", fuzzyset.linear({ [0] = 30, 35 }, { [0] = 0, 1 }))
 
-    local speed = variable.new("speed")
+    local speed = variable.new("speed", 0, 100)
     local stop = speed:add("stop", fuzzyset.linear({ [0] = 0, 20 }, { [0] = 1, 0 }))
     local slow = speed:add("slow", fuzzyset.linear({ [0] = 10, 30, 50 }, { [0] = 0, 1, 0 }))
     local medium = speed:add("medium", fuzzyset.linear({ [0] = 40, 60, 80 }, { [0] = 0, 1, 0 }))
@@ -60,7 +60,7 @@ test[#test + 1] = function()
     local answer = rb:infer(basis.mamdani)
     answer = answer:defuzzy()
 
-    assert(math.abs(41.42857142857142 - answer) < 1e-8)
+    assert(math.abs(45 - answer) < 1e-8)
 end
 
 return test
